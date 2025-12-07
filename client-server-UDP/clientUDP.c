71e3316c62d67b0ba08ff091c7ecc938cfe4741b
Aziz Harzallah <aziiz.harzallah@gmail.com>
2025-12-07 19:45:03 +0100

    update UDP

--- client-server-UDP/clientUDP.c
+++ client-server-UDP/clientUDP.c
@@ -4,12 +4,13 @@
 #include <time.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <netdb.h>
+#include <sys/socket.h>
 
-#define NMAX 10
-#define BUFSIZE 1024
+#define NMAX 10        /* Maximum number of random numbers to request */
+#define BUFSIZE 1024   /* Buffer size for messages */
 
 int main(int argc, char **argv) {
+    /* Validate command line arguments */
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <server-host> <server-port>\n", argv[0]);
         return 1;
@@ -18,43 +19,49 @@ int main(int argc, char **argv) {
     const char *host = argv[1];
     const char *port = argv[2];
 
-    /* resolve server address */
-    struct addrinfo hints, *res;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;        /* IPv4 */
-    hints.ai_socktype = SOCK_DGRAM;   /* UDP */
+    /* Create UDP socket (SOCK_DGRAM for datagram protocol) */
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) { perror("socket"); return 1; }
 
-    if (getaddrinfo(host, port, &hints, &res) != 0) {
-        perror("getaddrinfo");
+    /* Initialize server address structure */
+    struct sockaddr_in srv;
+    memset(&srv, 0, sizeof(srv));
+    srv.sin_family = AF_INET;                    /* IPv4 */
+    srv.sin_port = htons(atoi(port));           /* Convert port to network byte order */
+    
+    /* Convert IP address string to binary form */
+    if (inet_pton(AF_INET, host, &srv.sin_addr) <= 0) {
+        perror("inet_pton");
+        close(sock);
         return 1;
     }
 
-    int sock = socket(res->ai_family, res->ai_socktype, 0);
-    if (sock < 0) { perror("socket"); freeaddrinfo(res); return 1; }
-
-    /* generate random number */
+    /* Generate random number between 1 and NMAX */
     srand(time(NULL) ^ getpid());
     int n = (rand() % NMAX) + 1;
 
+    /* Format number as string for transmission */
     char buf[BUFSIZE];
     snprintf(buf, sizeof(buf), "%d", n);
 
-    /* send number */
-    if (sendto(sock, buf, strlen(buf)+1, 0, res->ai_addr, res->ai_addrlen) < 0) {
+    /* Send random number to server */
+    if (sendto(sock, buf, strlen(buf)+1, 0, (struct sockaddr*)&srv, sizeof(srv)) < 0) {
         perror("sendto");
-        close(sock); freeaddrinfo(res); return 1;
+        close(sock);
+        return 1;
     }
 
-    /* receive reply */
-    struct sockaddr_storage from;
+    /* Receive server response with n random numbers */
+    struct sockaddr_in from;
     socklen_t fromlen = sizeof(from);
     ssize_t r = recvfrom(sock, buf, sizeof(buf)-1, 0, (struct sockaddr*)&from, &fromlen);
-    if (r < 0) { perror("recvfrom"); close(sock); freeaddrinfo(res); return 1; }
+    if (r < 0) { perror("recvfrom"); close(sock); return 1; }
     buf[r] = '\0';
 
+    /* Display results */
     printf("Client sent n=%d\nServer replied: %s\n", n, buf);
 
+    /* Close socket and exit */
     close(sock);
-    freeaddrinfo(res);
     return 0;
 }
--- client-server-UDP/serveurUDP.c
+++ client-server-UDP/serveurUDP.c
@@ -1,4 +1,5 @@
 /* serveurUDP.c
+   UDP Server: listens on a port and responds to client requests
    Usage: ./serveurUDP <port>
 */
 #include <stdio.h>
@@ -9,42 +10,56 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define BUFSZ 2048
+#define BUFSZ 2048   /* Buffer size for messages */
 
 int main(int argc, char **argv) {
+    /* Validate command line arguments */
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         return 1;
     }
+    
+    /* Parse and validate port number */
     int port = atoi(argv[1]);
     if (port <= 0) { fprintf(stderr, "Bad port\n"); return 1; }
 
+    /* Create UDP socket (SOCK_DGRAM for datagram protocol) */
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0) { perror("socket"); return 1; }
 
+    /* Initialize server address structure */
     struct sockaddr_in srv;
     memset(&srv, 0, sizeof(srv));
-    srv.sin_family = AF_INET;
-    srv.sin_addr.s_addr = INADDR_ANY;
-    srv.sin_port = htons(port);
+    srv.sin_family = AF_INET;               /* IPv4 */
+    srv.sin_addr.s_addr = INADDR_ANY;      /* Listen on all available interfaces */
+    srv.sin_port = htons(port);            /* Convert port to network byte order */
 
+    /* Bind socket to address and port */
     if (bind(sock, (struct sockaddr*)&srv, sizeof(srv)) < 0) {
         perror("bind"); close(sock); return 1;
     }
 
     printf("UDP server listening on port %d\n", port);
+    
+    /* Initialize random number generator */
     srand(time(NULL));
 
     char buf[BUFSZ];
+    
+    /* Main server loop: accept and process client requests */
     for (;;) {
+        /* Receive data from client */
         struct sockaddr_in cli;
         socklen_t clilen = sizeof(cli);
         ssize_t r = recvfrom(sock, buf, sizeof(buf)-1, 0, (struct sockaddr*)&cli, &clilen);
         if (r <= 0) continue;
+        
+        /* Null-terminate received data and parse as integer */
         buf[r] = '\0';
         int n = atoi(buf);
         if (n <= 0) n = 1;
-        /* build response: n random numbers separated by space */
+        
+        /* Build response: n random numbers separated by space */
         char out[BUFSZ];
         out[0] = '\0';
         for (int i = 0; i < n; ++i) {
@@ -55,15 +70,18 @@ int main(int argc, char **argv) {
         }
         strncat(out, "\n", sizeof(out)-strlen(out)-1);
 
+        /* Send response back to client */
         if (sendto(sock, out, strlen(out), 0, (struct sockaddr*)&cli, clilen) < 0) {
             perror("sendto");
         } else {
+            /* Convert client IP address to string and log the request */
             char ip[INET_ADDRSTRLEN];
             inet_ntop(AF_INET, &cli.sin_addr, ip, sizeof(ip));
             printf("Request from %s:%d n=%d -> sent %d numbers\n", ip, ntohs(cli.sin_port), n, n);
         }
     }
 
+    /* Close socket and exit */
     close(sock);
     return 0;
 }
